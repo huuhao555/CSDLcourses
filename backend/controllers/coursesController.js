931ecccbbe4e3b56f0ca8dc972012d4d1e3b9b23
Hữu Hào <phanhao@Huus-MacBook-Air.local>
2024-11-05 13:17:54 +0700

    Add .DS_Store to .gitignore

--- backend/controllers/coursesController.js
+++ backend/controllers/coursesController.js
@@ -49,19 +49,38 @@ const getById = async (req, res) => {
     res.status(500).json({ message: "Failed to getById course" });
   }
 };
+const { v4: uuidv4 } = require("uuid"); // Import thư viện UUID để tạo requestId duy nhất
+
 const purchaseCourse = async (req, res) => {
   const courseId = req.params.id;
+  const requestId = uuidv4(); // Tạo một requestId duy nhất cho mỗi yêu cầu
+  console.log(
+    `[${new Date().toISOString()}] - Bắt đầu xử lý - Request ID: ${requestId}`
+  );
 
   try {
     const course = await Courses.findById(courseId);
+    console.log(
+      `[${new Date().toISOString()}] - Hoàn tất tìm kiếm khoá học - Request ID: ${requestId}`
+    );
 
     if (!course) {
+      console.log(
+        `[${new Date().toISOString()}] - Khoá học không tồn tại - Request ID: ${requestId}`
+      );
       return res.status(404).json({ message: "Course not found" });
     }
 
     if (course.quantityInStock <= 0) {
+      console.log(
+        `[${new Date().toISOString()}] - Khoá học đã hết hàng - Request ID: ${requestId}`
+      );
       return res.status(400).json({ message: "Course out of stock" });
     }
+
+    console.log(
+      `[${new Date().toISOString()}] - Bắt đầu cập nhật khoá học - Request ID: ${requestId}`
+    );
 
     const updatedCourse = await Courses.findOneAndUpdate(
       {
@@ -69,21 +88,38 @@ const purchaseCourse = async (req, res) => {
         quantityInStock: course.quantityInStock,
         __v: course.__v
       },
-      { $inc: { quantityInStock: -1 }, $inc: { __v: 1 } },
+      {
+        $inc: {
+          quantityInStock: -1,
+          __v: 1
+        }
+      },
       { new: true }
     );
 
     if (!updatedCourse) {
+      console.log(
+        `[${new Date().toISOString()}] - Xung đột, cập nhật thất bại - Request ID: ${requestId}`
+      );
       return res
         .status(409)
         .json({ message: "Conflict detected. Please try again." });
     }
+
+    console.log(
+      `[${new Date().toISOString()}] - Đặt khoá học thành công - Request ID: ${requestId}`
+    );
 
     res.status(200).json({
       message: "Course purchased successfully",
       course: updatedCourse
     });
   } catch (error) {
+    console.log(
+      `[${new Date().toISOString()}] - Lỗi xảy ra: ${
+        error.message
+      } - Request ID: ${requestId}`
+    );
     res.status(500).json({ message: "Failed to purchase course" });
   }
 };
